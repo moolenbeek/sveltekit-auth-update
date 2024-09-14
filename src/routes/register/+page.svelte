73730a7c89b85703861eab2124c89d358ffdc7a0
moolenbeek <emoolenbeek@gmail.com>
2024-09-14 15:11:54 -0400

    update form

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,16 +1,10 @@
-<script lang="ts">
-	import type { PageData } from "./$types";
-	export let data: PageData;
-	console.log(data);
-</script>
-
 <form method="POST">
 	<h2>Register</h2>
 	<label for="name">Name</label>
 	<input type="text" id="name" name="name" required />
 
-	<label for="username">Username</label>
-	<input type="text" id="username" name="username" required />
+	<label for="email">Email</label>
+	<input type="email" id="email" name="email" required />
 
 	<label for="password">Password</label>
 	<input type="password" id="password" name="password" required />
