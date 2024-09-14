b28c4cf95dfbfb382881cdec1852cb9bbe023aef
moolenbeek <emoolenbeek@gmail.com>
2024-09-14 15:11:08 -0400

    handle login

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,5 +1,25 @@
-import type { PageServerLoad } from "./$types";
+import { createUser } from "$lib/server/database/router/users";
 
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+import { fail, redirect } from "@sveltejs/kit";
+import type { Actions } from "./$types";
+
+export const actions: Actions = {
+	default: async function ({ request }) {
+		const { name, email, password } = Object.fromEntries(await request.formData()) as Record<
+			string,
+			string
+		>;
+
+		const id = crypto.randomUUID();
+		const token = crypto.randomUUID();
+		try {
+			await createUser({ id, email, name, password, token });
+		} catch (err) {
+			console.error(err);
+			return fail(400, { message: "Could not register user" });
+		}
+		throw redirect(302, "/login");
+	}
+};
+
+// create user sessions
